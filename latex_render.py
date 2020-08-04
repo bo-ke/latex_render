381bebaf72ed8c3217f60d674b4d07de7f2788b4
kebo <itachi971009@gmail.com>
2020-08-04 23:59:00 +0800

    [fea] add latex_render class

---
+++ latex_render.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+@author: kebo
+@contact: kebo0912@outlook.com
+@version: 1.0
+
+@file: latex_render.py
+@time: 2020/7/31 下午9:19
+
+这一行开始写关于本文件的说明与解释
+
+refer: https://www.zhihu.com/question/26887527
+"""
+
+import re
+from urllib.parse import quote
+
+GITHUB_RENDER_URL_BASE = 'https://render.githubusercontent.com/render/math?'
+CODECOGS_RENDER_URL = 'https://latex.codecogs.com/svg.latex?'
+
+
+class LatexRender:
+    """
+
+    """
+
+    def __init__(self, input_file: str, output_file: str = None, render_type: int = 1):
+        """
+
+        :param input_file:
+        :param output_file:
+        :param render_type: 1: use codecogs.com  2: use render.githubusercontent.com default: 1
+        """
+        self.input_file = input_file
+        self.output_file = output_file or input_file
+        self.render_type = render_type
+
+    def render(self):
+        """
+        render the latex element of input_file
+        :return:
+        """
+        text = open(self.input_file, encoding="utf-8").read()
+        parts = text.split("$$")
+        if type == 1:
+            for i, part in enumerate(parts):
+                if i & 1:
+                    parts[i] = self._latex_part(part=part.strip())
+
+        text_out = "\n\n".join(parts)
+
+        lines = text_out.split('\n')
+        for lid, line in enumerate(lines):
+            parts = re.split(r"\$(.*?)\$", line)
+            for i, part in enumerate(parts):
+                if i & 1:
+                    parts[i] = self._latex_part(part=part.strip())
+            lines[lid] = ' '.join(parts)
+        text_out = "\n".join(lines)
+
+        with open(self.output_file, "w", encoding='utf-8') as f:
+            f.write(text_out)
+
+    def _latex_part(self, part):
+        """
+
+        :param part:
+        :return:
+        """
+        if self.render_type == 1:
+            part = '![math](' + CODECOGS_RENDER_URL + part.strip()
+        elif type == 2:
+            part = '![math](' + GITHUB_RENDER_URL_BASE + 'math={})'.format(quote(part.strip()))
+        else:
+            raise ValueError("type mast be one of `1` and `2`, but got {}".format(self.render_type))
+        return part
