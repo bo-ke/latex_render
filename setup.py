b6ae662f05ff0c9ad1854b30026714f8549e11b0
kebo <itachi971009@gmail.com>
2020-08-05 22:20:11 +0800

    [mod] add overrides requirements

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[
-        'typing~=3.6.4'
+        'typing~=3.6.4',
+        'overrides~=2.8.0'
     ],
     entry_points={"console_scripts": ["latex_render=latex_render.__main__:run"]},
     python_requires='>=3.6.1',
